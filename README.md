7d48cfc60f0281bc926724b0610c9c1d20a06f01
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-03-07 12:32:43 -0500

    adding onclick

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # PasswordGenerator
+
+GIVEN I need a new, secure password
+
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria - yes
+
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password - yes
+
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters - yes
+
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters - yes
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected - yes
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
--- script.js
+++ script.js
@@ -3,23 +3,24 @@ console.log("testing")
 
 /*choosing the length of the password. If you choose outside the allotted amount of characters, 
 it will keep asking until you choose a number within the allotted amount of characters*/
+function myFunction(){
 do {
   var length = prompt("Choose a number for the length of your password between 8 and 128");
 } while (length < 8 || length > 128 );
 console.log(length);
-
-
 //choosing to include uppercase letters, will continue asking until at least one variable is chosen
 do {
-  var upperCase = confirm("Would you like to include uppercase letters (type T or F)");  
-  var lowerCase = confirm("Would you like to include lowercase letters (type T or F)");
-  var number = confirm("Would you like to include numbers (type T or F)");
-  var symbols = confirm("Would you like to include symbols (type T or F)");
-} while ( upperCase !== true && lowerCase !== true && number !== true && symbols !== true);
-  console.log(upperCase);
-  console.log(lowerCase);
-  console.log(number);
-  console.log(symbols);
+    var upperCase = confirm("Would you like to include uppercase letters (type T or F)");  
+    var lowerCase = confirm("Would you like to include lowercase letters (type T or F)");
+    var number = confirm("Would you like to include numbers (type T or F)");
+    var symbols = confirm("Would you like to include symbols (type T or F)");
+  } while ( upperCase !== true && lowerCase !== true && number !== true && symbols !== true);
+    console.log(upperCase);
+    console.log(lowerCase);
+    console.log(number);
+    console.log(symbols);
+};
+
 
 //generate password
 
